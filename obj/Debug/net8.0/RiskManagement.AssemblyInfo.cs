68be718b215559e06d070f3412a72e99874a3be7
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-17 16:07:37 +0300

    fixing layout problem

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d13f1e8c2bd393856f18f2ab1460a267bc54095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1388286950914439a876f346cb26b69ab5dd3cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
