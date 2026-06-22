c1e0cd65ead2f31580dc7b62038687759142de64
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-22 14:54:24 +0300

    add the dash board layout css to main layout

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64db133b809fff55bad5a4b7c266cd5703236b5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b4490f5bca6eb08aee79488faaf2cf51eb19e23")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
