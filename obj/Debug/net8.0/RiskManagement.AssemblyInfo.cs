a77fa3f76a40c46be048e59d5f1e28286d851c17
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-22 09:52:53 +0300

    installing boostrap icon

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2879b706cc80bc2f9ba5beb4c4150363bfa8b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb285c48dac675a2fb13baf214c551a3fc46c210")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
