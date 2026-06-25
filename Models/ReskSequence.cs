8678bd44ab63505e8125cd2450a7a10b848eb276
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-25 15:07:01 +0300

    adding one model for  acts as a counter for each risk type.

---
+++ Models/ReskSequence.cs
@@ -0,0 +1,11 @@
+namespace RiskManagement.Models
+{
+    public class ReskSequence
+    {
+        public int Id { get; set; }
+
+        public string Prefix { get; set; }
+
+        public int LastNumber { get; set; }
+    }
+}
--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e727eb3771c376bdf85896b72c04a678151ec741")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea457b692d300167a4cf6e37e8d2e89aebf2359")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
