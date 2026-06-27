2b0405e90ebd56fb98a951fe12fc71eec76f6219
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-27 09:16:51 +0300

    adding the correct js and css for data tables

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d125e50017f840f79f9320630854379e0dfd327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b5971dcc5ecca33769eaa66c413098be33c62a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
