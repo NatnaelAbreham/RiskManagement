3b5d2e83551853736692921a0cb57aa12a96b584
NatnaelAbreham <natiabreham728@gmail.com>
2026-06-30 09:49:18 +0300

    make the footer content center

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67080e0ce110ae0542af8adb0295b98b3d454454")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee19af1d5be79823b48a39c8b52c93543a5d10a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/editrisk.js
+++ wwwroot/js/editrisk.js
@@ -209,7 +209,7 @@ $(document).on('click', '.edit-btn', async function () {
 
         footerHtml = `
         <button type="button"
-                class="btn btn-primary"
+                class="btn btn-success"
                 data-bs-dismiss="modal">
             OK
         </button>
