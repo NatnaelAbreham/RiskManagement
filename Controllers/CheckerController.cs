ad5a2192430f0b78486d397549a3b56a1ec35d60
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-06 15:19:45 +0300

    adding dashboard route in the checker controller

--- Controllers/CheckerController.cs
+++ Controllers/CheckerController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RiskManagement.Data;
+using RiskManagement.Models;
 
 namespace RiskManagement.Controllers
 {
@@ -15,7 +16,7 @@ namespace RiskManagement.Controllers
         [HttpGet("View")]
         public IActionResult ViewRecord()
         {
-           // var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            // var email = User.FindFirst(ClaimTypes.Email)?.Value;
             var filteredUsers = _context.RiskRegistrations
           //.Where(u => u.RegisteredBy == email)
           .OrderByDescending(u => u.Id)
@@ -26,7 +27,33 @@ namespace RiskManagement.Controllers
         [HttpGet("Dashboard")]
         public IActionResult Dashboard()
         {
-            return View();
+            CheckerDashboard model = new CheckerDashboard();
+
+            model.TotalRisk = _context.RiskRegistrations.Count();
+
+            model.OpenRisk = _context.RiskRegistrations
+                .Count(x => x.Status == "Open");
+
+            model.ClosedRisk = _context.RiskRegistrations
+                .Count(x => x.Status == "Closed");
+
+            model.HighRisk = _context.RiskRegistrations
+                .Count(x => x.RiskRating == "High");
+
+            model.DueSoon = _context.RiskRegistrations
+                .Count(x => x.MitigationPlannedDate <= DateTime.Today.AddDays(7));
+
+            model.RecentRisks = _context.RiskRegistrations
+                .OrderByDescending(x => x.RiskDate)
+                .Take(10)
+                .ToList();
+
+            model.TopRisk = _context.RiskRegistrations
+                .OrderByDescending(x => x.RiskScore)
+                .Take(10)
+                .ToList();
+
+            return View(model);
         }
 
         [HttpGet("Profile")]
