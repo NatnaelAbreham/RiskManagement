8f48314fe63f8a794046a8aef72b7eb1fcd14b7b
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-06 14:54:01 +0300

    add http get to checker contoller

--- Controllers/CheckerController.cs
+++ Controllers/CheckerController.cs
@@ -1,14 +1,39 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RiskManagement.Data;
 
 namespace RiskManagement.Controllers
 {
+    [Route("Checker")]
     [Authorize(Roles = "Checker")]
     public class CheckerController : Controller
     {
+
+        public CheckerController(AppDBContext context) => _context = context;
+        private readonly AppDBContext _context;
+
+        [HttpGet("View")]
+        public IActionResult ViewRecord()
+        {
+           // var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            var filteredUsers = _context.RiskRegistrations
+          //.Where(u => u.RegisteredBy == email)
+          .OrderByDescending(u => u.Id)
+          .ToList();
+            return View("View", filteredUsers);
+        }
+
+        [HttpGet("Dashboard")]
         public IActionResult Dashboard()
         {
             return View();
         }
+
+        [HttpGet("Profile")]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
     }
 }
