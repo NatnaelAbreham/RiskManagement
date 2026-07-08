f85ca82b40e82f00f64f423cf3d724ef7f4e7c9c
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-08 13:42:24 +0300

    make the approvedby variable to null in the approval model

--- Models/ApproveRequest.cs
+++ Models/ApproveRequest.cs
@@ -3,6 +3,6 @@ namespace RiskManagement.Models
     public class ApproveRequest
     {
         public string RiskId { get; set; }
-        public string ApprovedBy { get; set; }
+        public string? ApprovedBy { get; set; }
     }
 }
