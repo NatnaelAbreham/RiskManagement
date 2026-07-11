4d3d1d59e51b13dc466247cfa74f1b8d34701937
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-11 09:56:35 +0300

    adding library jszip.min.js for excel export

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b16d96b7d248dbec78b5be9b5f533af62ca904a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13b22a54d59c4df328b78c3e4d6531c973415f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
