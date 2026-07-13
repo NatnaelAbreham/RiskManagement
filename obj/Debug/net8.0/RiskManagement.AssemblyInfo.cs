2fe01e55e03e6a707bccfc885c4f9a6c99657b30
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-13 14:38:47 +0300

    installing js font for pdf

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42584fad6ef0c9cc1192f267d938c083e0a879c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39312bb225a0b6d929aa1498e84a04fce0c29bdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
