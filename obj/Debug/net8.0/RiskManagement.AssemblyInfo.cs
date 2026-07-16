5c533d25d3ca5e6d80217413313ca2a0eb8a7068
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-16 15:08:09 +0300

    make the code clean and indent

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e4f05444a7eb12dcf65ca7c77ffb27098e6990f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b011f4709f9ffb7e8da647aebcf827f009d9c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
