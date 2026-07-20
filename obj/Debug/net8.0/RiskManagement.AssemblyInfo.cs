95fd7e4211223fa1163838064ead882e9a1beae4
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-20 10:56:09 +0300

    change the dropdown to toggle for user status

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9db30e8beb25872c265c3ce72740f39ba1681b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c87657d4c41baabbadb6c7f5f5e441daa031f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
