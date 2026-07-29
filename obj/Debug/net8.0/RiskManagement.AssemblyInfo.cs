051d0afe20ec4043c2efceb187fb081baf009306
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-29 11:10:39 +0300

    fixing all issue related to the toggle

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0704630fb9210e55d5891e59070fab27a376187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e5ec831be91ee2f0b933bb48552a41a56e4c25d")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
