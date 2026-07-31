1731180e3ed2b9da6462692c7e9e023e668ab761
NatnaelAbreham <natiabreham728@gmail.com>
2026-07-31 15:21:00 +0300

    make all the field and dropdown mandatory

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a705a3db8d5d761ba03f17db1e8812402a1ada80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532e4b9fdd890e1758a701dc33680e29ee57e0d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
