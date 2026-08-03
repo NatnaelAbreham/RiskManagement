4d5f3a75df1971c3f2ea8224f85ca48c7d034741
NatnaelAbreham <natiabreham728@gmail.com>
2026-08-03 14:48:25 +0300

    adding file path to app setting

--- obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiskManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb753eb35a5b59317cb4959e26c92e1bd8862e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1845de8a4acf7998a73b7032ddb5ead58b908060")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiskManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
